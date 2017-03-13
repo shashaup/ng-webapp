2b2868741c2f2c7d84ec5affa3383fe984cf7a86
莎呱y <zyanzi1607@163.com>
2017-03-13 22:32:59 +0800

    完成了搜索的页面及其样式设置

---
+++ src/script/controller/searchCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('app').controller('searchCtrl',['$http','$scope',function($http,$scope){
+  $http.get('data/positionList.json').then(function(resp){
+    $scope.positionList = resp.data;
+  });
+}]);
