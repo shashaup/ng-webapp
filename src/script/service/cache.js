6ed2c57a3a3a7b8900ee8979036809e90fa9fb86
莎呱y <zyanzi1607@163.com>
2017-03-15 22:58:53 +0800

    完成了用户模块的部分逻辑

--- src/script/service/cache.js
+++ src/script/service/cache.js
@@ -1,3 +1,18 @@
+// "use strict";
+// angular.module("app").factory("cache", ["$cookies", function($cookies){
+// 	return {
+// 		put : function(key, value){
+// 			$cookies.put(key, value);
+// 		},
+// 		get : function(key){
+// 			return $cookies.get(key);
+// 		},
+// 		remove : function(key){
+// 			$cookies.remove(key);
+// 		},
+// 	};
+// }]);
+
 'use strict';
 angular.module("app").service('cache',['$cookies',function($cookies){
   this.put = function(key, value){
