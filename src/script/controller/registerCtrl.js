f83cfb81ff94d4afe00e3eb35647df44a0641e03
莎呱y <zyanzi1607@163.com>
2017-03-16 22:31:26 +0800

    完成了用户模块收藏及表单校验的逻辑编写

--- src/script/controller/registerCtrl.js
+++ src/script/controller/registerCtrl.js
@@ -1,5 +1,28 @@
 'use strict';
 angular.module('app')
-.controller('registerCtrl',['$http','$scope',function($http,$scope){
-
+.controller('registerCtrl',['$interval','$http','$scope','$state',function($interval,$http,$scope,$state){
+  $scope.submit = function() {
+    $http.get('data/regist.json',$scope.user).then(function(resp){
+      console.log(resp.data);
+      $state.go('login');
+    });
+  };
+  var count = 60;
+  $scope.send = function() {
+    $http.get('data/code.json').then(function(resp){
+      if(1===resp.data.state) {
+        count = 60;
+        $scope.time = '60s';
+        var interval = $interval(function() {
+          if(count<=0) {
+            $interval.cancel(interval);
+            $scope.time = '';
+          }else {
+            count--;
+            $scope.time = count + 's';
+          }
+        },1000);
+      }
+    });
+  }
 }]);
